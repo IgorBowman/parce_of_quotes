bf966841d85cc3d5a436c6ccd9a6afe66f185658
IgorBowman <xxx@gmaol.com>
2022-05-15 09:27:36 +0500

    parcer

---
+++ parcing/get_quotes.py
@@ -0,0 +1,52 @@
+from requests import Session
+from bs4 import BeautifulSoup
+
+
+headers = {"User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 12_0 like Mac OS X)"
+                         " AppleWebKit/605.1.15 (KHTML, like Gecko)"
+                         " CriOS/69.0.3497.105 Mobile/15E148 Safari/605.1"}
+
+work = Session()
+url = "https://quotes.toscrape.com/"
+
+work.get(url, headers=headers)
+response = work.get(url + "login", headers=headers)
+
+soup = BeautifulSoup(response.text, 'lxml')
+token = soup.find("form").find('input').get("value")
+
+data = {
+    'csrf_token': token,
+    'username': "user1",
+    'password': "user1auth",
+}
+
+result = work.post(url + "login", headers=headers, data=data, allow_redirects=True)
+
+count = 1
+
+while True:
+    start_url = f"{url}page/{count}/"
+
+    response = work.get(start_url, headers=headers)
+    soup = BeautifulSoup(response.text, 'lxml')
+    article = soup.find_all("div", class_="quote")
+
+    for item in article:
+        with open("get_all_quotes.txt", 'a', encoding="utf-8") as f:
+            text = item.find("span", class_="text").text
+            author = item.find("small", class_="author").text
+            for_tags = item.find_all("a", class_="tag")
+            tags = []
+
+            for i in for_tags:
+                tags.append(i.text)
+            f.write(f"Text: {text}" + "\n")
+            f.write(f"Author: {author}" + "\n")
+            f.write(f"Tags: {tags}")
+            f.write("\n\n")
+
+    next_button = soup.find("li", class_="next")
+    if not next_button:
+        break
+    count += 1
